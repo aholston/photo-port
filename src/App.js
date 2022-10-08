b352f417d72601e65123e3e5e5ad8421ca1225cb
Andre Holston <andreholston@Andres-MBP.lan>
2022-10-08 12:47:39 -0500

    add hooks to pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import './App.css';
 import './components/About';
 import About from './components/About';
 import Navigation from './components/Navigation';
+import Gallery from "./components/Gallery";
+import React, { useState } from 'react';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Navigation />
+      <Navigation 
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      />
       <main>
+        <Gallery />
         <About />
       </main>
     </div>
