66ff7dd1278690479c827fae8541476fbc5b521f
Andre Holston <andreholston@Andres-MBP.lan>
2022-10-08 17:35:07 -0500

    styling for contact form

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -25,13 +26,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       />
       <main>
-        <ContactForm />
-        <Gallery 
-          currentCategory={currentCategory}
-        />
-        <About />
+      {!contactSelected ? (
+        <>
+          <Gallery currentCategory={currentCategory}></Gallery>
+          <About></About>
+        </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
